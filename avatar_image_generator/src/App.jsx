60c4b4f9d98ce2682bf37f0830516ee774abd1f9
22piyush <piyushaglawe05@gmail.com>
2026-03-30 20:37:59 +0530

    done

--- avatar_image_generator/src/App.jsx
+++ avatar_image_generator/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen text-white flex items-center justify-center bg-gradient-to-br from bg-slate-900 via-slate-800 to-slate-900">
+      <div className="w-full max-w-md rounded-2xl shadow-xl backdrop-blur-xl border border-slate-700 p-10">
+            
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
