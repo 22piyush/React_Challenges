a3444f1db05b8217ce39e021153e87e93e705fac
22piyush <piyushaglawe05@gmail.com>
2026-03-30 21:33:37 +0530

    done

--- avatar_image_generator/src/App.jsx
+++ avatar_image_generator/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "animate.css";
 
 const data = [
@@ -50,11 +50,52 @@ const data = [
 ];
 
 function App() {
+  const [src, setSrc] = useState(null);
+  const [option, setOption] = useState("male");
+
+  const geneRandomHundred = () => {
+    return Math.floor(Math.random() * 99) + 1;
+  };
+
+  const generateAssets = () => {
+    const findOption = data.find((val) => val.value == option);
+    const url = findOption.url;
+
+    if (option == "male" || option == "female") {
+      const imgUrl = `${url}/${geneRandomHundred()}.jpg`;
+      setSrc(imgUrl);
+    } else {
+      const uniqueValue = Date.now();
+      const imageUrl = `${url}${uniqueValue}`;
+      setSrc(imageUrl);
+    }
+  };
+
+  const onOptionChange = (e) => {
+    const value = e.target.value;
+    setOption(value);
+  };
+
+  const download = (url) => {
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `${Date.now()}.jpg`;
+    a.click();
+    a.remove();
+  };
+
+  useEffect(() => {
+    generateAssets();
+  }, [option]);
+
   return (
     <div className="animate__animated animate__fadeIn overflow-hidden min-h-screen text-white flex items-center justify-center bg-gradient-to-br from bg-slate-900 via-slate-800 to-slate-900">
       <div className="animate__animated animate__bounceIn w-full max-w-md flex flex-col items-center rounded-2xl shadow-xl backdrop-blur-xl border border-slate-700 p-10">
         <img
-          src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4841.jpg?semt=ais_incoming&w=740&q=80"
+          src={
+            src ??
+            "https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4841.jpg?semt=ais_incoming&w=740&q=80"
+          }
           className="w-32 h-32 rounded-full border-4 border-slate-700 shadow-lg object-cover"
         />
 
@@ -66,7 +107,11 @@ function App() {
         </div>
 
         <div className="w-full mt-10 flex flex-col gap-5">
-          <select className="bg-slate-900/60 w-full p-3 rounded-xl">
+          <select
+            className="bg-slate-900/60 w-full p-3 rounded-xl"
+            value={option}
+            onChange={onOptionChange}
+          >
             {data.map((item, index) => (
               <option key={index} value={item.value}>
                 {item.label}
@@ -74,19 +119,31 @@ function App() {
             ))}
           </select>
 
-          <div className="bg-slate-900/60 w-full p-3 rounded-xl">coing ott</div>
+          <div className="bg-slate-900/60 w-full p-3 rounded-xl">{src}</div>
         </div>
 
         <div className="w-full mt-5 flex gap-4">
-          <button className="flex-1 bg-gradient-to-r from-rose-500 to-orange-600 font-medium rounded-lg py-2 cursor-pointer hover:scale-105 transition-transform">
+          <button
+            onClick={generateAssets}
+            className="flex-1 bg-gradient-to-r from-rose-500 to-orange-600 font-medium rounded-lg py-2 cursor-pointer hover:scale-105 transition-transform"
+          >
             Change
           </button>
 
-          <button className="flex-1 bg-gradient-to-r from-rose-500 to-orange-600 font-medium rounded-lg py-2 cursor-pointer hover:scale-105 transition-transform">
+          <button
+            onClick={() => download(src)}
+            className="flex-1 bg-gradient-to-r from-rose-500 to-orange-600 font-medium rounded-lg py-2 cursor-pointer hover:scale-105 transition-transform"
+          >
             Download
           </button>
 
-          <button className="flex-1 bg-gradient-to-r from-rose-500 to-orange-600 font-medium rounded-lg py-2 cursor-pointer hover:scale-105 transition-transform">
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText(src);
+              alert("Copied!");
+            }}
+            className="flex-1 bg-gradient-to-r from-rose-500 to-orange-600 font-medium rounded-lg py-2 cursor-pointer hover:scale-105 transition-transform"
+          >
             Copy
           </button>
         </div>
