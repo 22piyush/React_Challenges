52ab8fceea6bfa0ae34fd88f8811d601413590e6
22piyush <piyushaglawe05@gmail.com>
2026-03-31 22:15:12 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import ImageFinder from "./components/ImageFinder";
+import GradientFenerator from "./components/GradientFenerator";
+import AvatarImageGenerator from "./components/AvatarImageGenerator";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/image-finder" element={<ImageFinder/>} />
+        <Route path="/image-finder" element={<ImageFinder />} />
+        <Route path="/gradient-generator" element={<GradientFenerator />} />
+        <Route
+          path="/avatar-image-generator"
+          element={<AvatarImageGenerator />}
+        />
       </Routes>
     </BrowserRouter>
   );
