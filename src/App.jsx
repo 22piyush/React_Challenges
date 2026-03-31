0cd8d8939eafb6102eeda03a04bc9c6ea05e7514
22piyush <piyushaglawe05@gmail.com>
2026-03-31 22:13:34 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import ImageFinder from "./components/ImageFinder";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/image-finder" element={<ImageFinder/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
