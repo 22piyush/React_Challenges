ba4beeddf4fa1adc62abd13362170409d73f76a9
22piyush <piyushaglawe05@gmail.com>
2026-03-31 22:38:23 +0530

    done

--- src/config/data.js
+++ src/config/data.js
@@ -1,15 +1,15 @@
 export const data = [
     {
         title: "Image Finder",
-        id: "image_finder"
+        id: "image-finder"
     },
     {
         title: "Avatar Image Generator",
-        id: "avatar_image_generator"
+        id: "avatar-image-generator"
     },
     {
         title: "Gradient Generator",
-        id: "gradient_generator"
+        id: "gradient-generator"
     },
     
 ]
