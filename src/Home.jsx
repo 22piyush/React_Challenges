ffbd05e196301bccb29977ee27e27bea6bdc4229
22piyush <piyushaglawe05@gmail.com>
2026-03-31 22:24:02 +0530

    done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { data } from "./config/data";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="min-h-screen w-full bg-gradient-to-r from-purple-500 to-purple-800 text-white">
+      <div className="grid md:grid-cols-5 gap-3">
+        {data.map((item, index) => (
+          <div key={index}></div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/config/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        title: "Image Finder",
+        id: "image_finder"
+    },
+    {
+        title: "Avatar Image Generator",
+        id: "avatar_image_generator"
+    },
+    {
+        title: "Gradient Generator",
+        id: "gradient_generator"
+    },
+]
