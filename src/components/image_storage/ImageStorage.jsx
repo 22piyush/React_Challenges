94b160b18cb392849100b5f0ff939c7cbc88fc4f
22piyush <piyushaglawe05@gmail.com>
2026-04-13 22:54:06 +0530

    done

--- src/components/image_storage/ImageStorage.jsx
+++ src/components/image_storage/ImageStorage.jsx
@@ -44,6 +44,7 @@ function ImageStorage() {
         <div className="relative mx-auto w-8/12 hover:scale-115 transition-transform duration-300 hover:shadow-lg cursor-pointer text-white flex flex-col items-center gap-3  p-20 bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 rounded-xl">
           <Upload className="w-16 h-16" />
           <h1 className="text-xl font-medium">Click me to add an image</h1>
+          <p>Upload images to store on local storage</p>
           <input
             type="file"
             className="cursor-pointer absolute top-0 left-0 opacity-0 w-full h-full rounded-xl"
