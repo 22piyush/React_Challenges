1850156e2202c3aaf2be3033508898e589bb70b0
22piyush <piyushaglawe05@gmail.com>
2026-04-14 23:16:55 +0530

    done

--- src/components/ai_chat_bot/AIChatBot.jsx
+++ src/components/ai_chat_bot/AIChatBot.jsx
@@ -6,6 +6,13 @@ function AIChatBot() {
     <div className="bg-gray-200 min-h-screen">
       <div className="w-9/12 mx-auto bg-white min-h-screen">
         <h1 className="text-3xl font-bold text-center">✈️ AI Chatbot</h1>
+        <div className="p-8">
+          <div className="flex justify-start">
+            <div className="bg-rose-100  text-black font-medium px-4 py-3 rounded-xl w-fit">
+              Hi, how are you
+            </div>
+          </div>
+        </div>
         <div className="bg-indigo-600 p-8 fixed bottom-0 w-9/12">
           <form className="flex gap-4 ">
             <input
