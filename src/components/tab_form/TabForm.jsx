182e8c9ae18ecdcd8c783384a7bc1dcd65e62044
22piyush <piyushaglawe05@gmail.com>
2026-04-16 21:12:45 +0530

    done

--- src/components/tab_form/TabForm.jsx
+++ src/components/tab_form/TabForm.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import Profile from "./Profile";
+import Interest from "./Interest";
+import Setting from "./Setting";
+import "./tabForm.css";
 
 function TabForm() {
+  const tabs = [
+    {
+      name: "Profile",
+      component: Profile,
+    },
+    {
+      name: "Interest",
+      component: Interest,
+    },
+    {
+      name: "Setting",
+      component: Setting,
+    },
+  ];
+
   return (
     <div className="min-h-screen bg-slate-200">
-      <div></div>
+      {tabs.map((item, index) => (
+        <div key={index} class="header">
+          <div class="header-text">{item.name}</div>
+        </div>
+      ))}
     </div>
   );
 }
