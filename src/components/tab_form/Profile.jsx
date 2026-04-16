8038916e4907996b96b5093615b57d3c6891ce19
22piyush <piyushaglawe05@gmail.com>
2026-04-16 21:45:34 +0530

    done

--- src/components/tab_form/Profile.jsx
+++ src/components/tab_form/Profile.jsx
@@ -12,33 +12,35 @@ function Profile({ data, setData }) {
   };
 
   return (
-    <div>
-      <div>
-        <label>Name: </label>
-        <input
-          type="text"
-          className=""
-          value={name}
-          onChange={(e) => handleDataChange(e, "name")}
-        />
-      </div>
-      <div>
-        <label>Emai: </label>
-        <input
-          type="text"
-          className=""
-          value={email}
-          onChange={(e) => handleDataChange(e, "email")}
-        />
-      </div>
-      <div>
-        <label>Age: </label>
-        <input
-          type="text"
-          className=""
-          value={age}
-          onChange={(e) => handleDataChange(e, "email")}
-        />
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full h-full">
+        <div>
+          <label>Name: </label>
+          <input
+            type="text"
+            className="input"
+            value={name}
+            onChange={(e) => handleDataChange(e, "name")}
+          />
+        </div>
+        <div>
+          <label>Emai: </label>
+          <input
+            type="text"
+            className="input"
+            value={email}
+            onChange={(e) => handleDataChange(e, "email")}
+          />
+        </div>
+        <div>
+          <label>Age: </label>
+          <input
+            type="text"
+            className="input"
+            value={age}
+            onChange={(e) => handleDataChange(e, "email")}
+          />
+        </div>
       </div>
     </div>
   );
