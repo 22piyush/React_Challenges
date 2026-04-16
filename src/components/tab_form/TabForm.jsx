ef8dc040f661a1ff967d7545e68de0f7011cb90c
22piyush <piyushaglawe05@gmail.com>
2026-04-16 21:22:40 +0530

    done

--- src/components/tab_form/TabForm.jsx
+++ src/components/tab_form/TabForm.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "./Profile";
 import Interest from "./Interest";
 import Setting from "./Setting";
 import "./tabForm.css";
 
 function TabForm() {
+  const [activeTab, setActiveTab] = useState(0);
+
   const tabs = [
     {
       name: "Profile",
@@ -20,13 +22,20 @@ function TabForm() {
     },
   ];
 
+  const ActiveTabComponent = tabs[activeTab].component;
+
   return (
-    <div className="min-h-screen bg-slate-200">
-      {tabs.map((item, index) => (
-        <div key={index} class="header">
-          <div class="header-text">{item.name}</div>
-        </div>
-      ))}
+    <div className="min-h-screen p-10 bg-slate-200 flex flex-col">
+      <div class="header">
+        {tabs.map((item, index) => (
+          <div key={index} class="header-text" onClick={()=>setActiveTab(index)}>
+            {item.name}
+          </div>
+        ))}
+      </div>
+      <div class="header-text flex-1 mt-10">
+        <ActiveTabComponent />
+      </div>
     </div>
   );
 }
